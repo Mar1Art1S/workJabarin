cef22fc0a653436f403dc3c4ad705515c38288eb
Oleg Livinskyi <83884642+Mar1Art1S@users.noreply.github.com>
2021-10-11 09:11:31 +0300

    jsa

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,21 @@
         $(this).toggleClass("active");
         $("#nav").toggleClass("active");
     });
+
+    document.getElementsByTagName("body")[0].onload = function(){ vpSize();}
+        document.getElementsByTagName("body")[0].onresize = function(){ vpSize();}
+
+        var cnt = 0;
+        function vpSize() {
+                cnt += 1;
+            var w = document.documentElement.clientWidth,
+                h = document.documentElement.clientHeight,
+                screenW = screen.width,
+                screenH = screen.height;
+
+            //document.getElementById("p1").innerHTML = w + "x" + h;
+            document.getElementById("vpPara").innerHTML = "Viewport Resolution :" +  w + "x" + h;
+            document.getElementById("vpSpan").innerHTML = "resize called "+ cnt +" times";
+            document.getElementById("screenPara").innerHTML = "Screen Resolution :" +  screenW + "x" + screenH;
+        }
+
